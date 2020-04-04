84adceaa967d702b825f1ce000123f07ddbeefc0
mcerrillo1 <mcerrillo1@yahoo.com>
2020-04-03 20:15:10 -0700

    new files added for skeleton

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,10 @@
+// TODO: Return markdown string for README file given a data object.
+function generateMarkdown(data) {
+    return `
+  # ${data.title}
+  
+  `;
+  }
+  
+  module.exports = generateMarkdown;
+  
